5a5082656c42fa171f16c51aebf48acab3d94203
Reem Hassan <reemhassan742r@gmail.com>
2026-02-09 07:14:09 +0300

    Exploring JSX thing and starting the app to see how it works "Display"..

---
+++ React/src/Components/Card/Card.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Card.css'
+type Props = {}
+
+const Card = (props: Props) => {
+  return (
+    <div className='card'>
+      <h2>Card Title</h2>
+
+      <div className='ditails'>
+        <h2>AAPL</h2>
+        <p>$110</p>
+      </div>
+        <p className='info'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate.</p>
+       
+    </div>
+  )
+}
+
+export default Card
