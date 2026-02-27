a4a25ef15fa4ecf29092ab77be6c939ad3d93966
Reem Hassan <reemhassan742r@gmail.com>
2026-02-27 23:53:40 +0300

    Finished the Models

---
+++ API/API/Models/Comment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Models
+{
+    [Table("Comments")]
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public int? StockId { get; set; }
+        public Stock? Stock { get; set; }
+        public string UserId { get; set; } = string.Empty;
+        public AppUser? User { get; set; }
+    }
+}
